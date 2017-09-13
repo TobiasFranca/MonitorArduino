698e279961699db0ed0db001e541b1b7f0296d5c
Tobias França <tobiasfranca.info@gmail.com>
2017-09-12 22:02:05 -0300

    Adicionado Projeto Arduino

---
+++ MonitorArduino/SerialArduino/SerialArduino.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		SerialArduino.ino
+ Created:	12/09/2017 22:01:30
+ Author:	tobia
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
