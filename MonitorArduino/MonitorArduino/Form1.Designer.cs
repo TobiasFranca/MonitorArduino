b1ff1cd28b53000bb46298d516b303ea74a19dab
Tobias França <tobiasfranca.info@gmail.com>
2017-09-12 21:42:04 -0300

    Componentes da tela

--- MonitorArduino/MonitorArduino/Form1.Designer.cs
+++ MonitorArduino/MonitorArduino/Form1.Designer.cs
@@ -29,11 +29,89 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.txtEnviar = new System.Windows.Forms.TextBox();
+            this.txtReceber = new System.Windows.Forms.TextBox();
+            this.timerCOM = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(12, 12);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(75, 23);
+            this.btnConectar.TabIndex = 0;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(93, 14);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(106, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(12, 56);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviar.TabIndex = 2;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            // 
+            // txtEnviar
+            // 
+            this.txtEnviar.Location = new System.Drawing.Point(93, 58);
+            this.txtEnviar.Name = "txtEnviar";
+            this.txtEnviar.Size = new System.Drawing.Size(235, 20);
+            this.txtEnviar.TabIndex = 3;
+            // 
+            // txtReceber
+            // 
+            this.txtReceber.Location = new System.Drawing.Point(12, 85);
+            this.txtReceber.Multiline = true;
+            this.txtReceber.Name = "txtReceber";
+            this.txtReceber.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtReceber.Size = new System.Drawing.Size(316, 189);
+            this.txtReceber.TabIndex = 4;
+            // 
+            // timerCOM
+            // 
+            this.timerCOM.Interval = 1000;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(339, 286);
+            this.Controls.Add(this.txtReceber);
+            this.Controls.Add(this.txtEnviar);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnConectar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Monitor Arduino";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.TextBox txtEnviar;
+        private System.Windows.Forms.TextBox txtReceber;
+        private System.Windows.Forms.Timer timerCOM;
+        private System.IO.Ports.SerialPort serialPort1;
     }
 }
 
--- MonitorArduino/MonitorArduino/Form1.cs
+++ MonitorArduino/MonitorArduino/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -14,6 +15,48 @@ namespace MonitorArduino
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void atualizaListaCOMs()
+        {
+            int i;
+            bool quantDiferente; //flag para sinalizar que a quantidade de portas mudou
+
+            i = 0;
+            quantDiferente = false;
+
+            //se a quantidade de portas mudou
+            if (comboBox1.Items.Count == SerialPort.GetPortNames().Length)
+            {
+                foreach (string s in SerialPort.GetPortNames())
+                {
+                    if (comboBox1.Items[i++].Equals(s) == false)
+                    {
+                        quantDiferente = true;
+                    }
+                }
+            }
+            else
+            {
+                quantDiferente = true;
+            }
+
+            //Se não foi detectado diferença
+            if (quantDiferente == false)
+            {
+                return;                     //retorna
+            }
+
+            //limpa comboBox
+            comboBox1.Items.Clear();
+
+            //adiciona todas as COM diponíveis na lista
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                comboBox1.Items.Add(s);
+            }
+            //seleciona a primeira posição da lista
+            comboBox1.SelectedIndex = 0;
         }
     }
 }
